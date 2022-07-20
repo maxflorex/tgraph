ae86468463c7392fcb3de6138aa86aa9d040b74b
Max Flores <max@maxflores.dev>
2022-07-20 07:29:16 -0600

    Mongo DB & Schemas

--- server/index.js
+++ server/index.js
@@ -1,17 +1,24 @@
 const express = require('express')
+const colors = require('colors')
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql')
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+
+// CONNECT TO DB
+connectDB()
 
 // DEFINE PORT TO LINSTEN TO
 const port = process.env.PORT || 5000
 // RUN EXPRESS SERVER
 const app = express()
 
+// GRAPHQL PLAYGROUND
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
 }))
 
+// PORT LISTENER
 app.listen(port, console.log(`Server running on port ${port}`))
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,28 @@
 const { projects, clients } = require('../schema/sampleData')
+const Project = require('../models/Project')
+const Client = require('../models/Client')
 
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema } = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLList, GraphQLNonNull } = require('graphql')
 
+// PROJECT TYPE
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        // ADDING RELATIONSHIPS TO ANOTHER DB
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.findById(parent.clientId)
+            }
+        }
+    })
+})
+
+// CLIENT TYPE
 const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
@@ -14,17 +35,71 @@ const ClientType = new GraphQLObjectType({
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
-    fields: () => ({
+    fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, arrg) {
+                return Project.find()
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return Project.findById(args.id)
+            }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, arrg) {
+                return Client.find()
+            }
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id)
+                return Client.findById(args.id)
             }
         }
-    })
+    }
 })
 
-module.export = new GraphQLSchema({
-    query: RootQuery
+// MUTATIONS
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        // ADD CLIENT
+        addClient: {
+            type: ClientType,
+            args: {
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+                phone: { type: GraphQLString },
+            },
+            resolve(parent, args) {
+                const client = new Client({
+                    name: args.name,
+                    email: args.email,
+                    phone: args.phone,
+                });
+                return client.save()
+            }
+        },
+        // DELECTE CLIENT
+        deleteClient: {
+            type: ClientType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) }
+            },
+            resolve(parent, args) {
+                return Client.findByIdAndRemove(args.id)
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation
 })
